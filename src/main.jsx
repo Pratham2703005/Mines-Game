7e58d883868c10c8280c763cf78bf849fcb08287
Pratham Israni <pk2732004@gmail.com>
2024-10-21 20:42:32 +0530

    try to make app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import the file
 
+// Mount the root of the app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+// Register the service worker for PWA
+serviceWorkerRegistration.register();
